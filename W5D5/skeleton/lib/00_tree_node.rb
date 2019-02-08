8c43281fb9b22390156a0b6e78649a776daf6049
Colin <colin.reitman87@gmail.com>
2019-02-08 16:43:07 -0500

    finished tree work

--- W5D5/skeleton/lib/00_tree_node.rb
+++ W5D5/skeleton/lib/00_tree_node.rb
@@ -2,56 +2,69 @@
 
 class PolyTreeNode
 
-    attr_reader :value, :parent, :children
+  attr_reader :value, :parent, :children
 
 
-   def initialize(value)
-    
-    @parent = nil
-    @children = []
-    @value = value
-    
-   end
+  def initialize(value)
+  
+  @parent = nil
+  @children = []
+  @value = value
+  
+  end
 
-    def parent=(node)
+  def parent=(node)
 
-        if !@parent.nil? && @parent.children.include?(self)
-            @parent.children.delete(self)
-        end
-
-        if !node.nil? && !node.children.include?(self) 
-            node.children << self
-        end
-
-        @parent = node
-    
+    if !@parent.nil? && @parent.children.include?(self)
+      @parent.children.delete(self)
     end
 
-
-    def add_child(node)
-        if !@children.include?(node)
-            node.parent = self
-            @children << node if !@children.include?(node)
-        end
+    if !node.nil? && !node.children.include?(self) 
+      node.children << self
     end
 
-    def remove_child(node)
-
-        raise "The node is already parentless" if node.parent.nil?
-        node.parent = nil
-        @children.delete(node)
+    @parent = node
+  
+  end
 
 
-
-        
-        if !@children.include?(node)
-            raise "error" 
-        else
-            node.parent = nil
-            @children.delete(node)
-        end
-
+  def add_child(node)
+    if !@children.include?(node)
+      node.parent = self
+      @children << node if !@children.include?(node)
     end
+  end
 
+  def remove_child(node)
+    raise "The node is already parentless" if node.parent.nil?
+    node.parent = nil
+  end
+
+  def dfs(target)
+    return nil if self.nil?
+    return self if self.value == target
+
+      self.children.each do |child|
+        result = child.dfs(target)
+        return result unless result.nil?
+      end
+    
+    nil
+
+  end
+
+  def bfs(target)
+
+    queue = [self]
+
+    while !queue.empty?
+        child = queue.shift
+        return child if child.value == target
+        queue.concat(child.children)
+    end
+    
+    nil
+
+  end
 
 end
