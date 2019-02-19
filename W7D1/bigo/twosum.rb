34b4126573513602cb2126337764d06c17c29d73
Colin <colin.reitman87@gmail.com>
2019-02-18 19:38:11 -0500

    inward walk two_sum

--- W7D1/bigo/twosum.rb
+++ W7D1/bigo/twosum.rb
@@ -12,11 +12,21 @@ end
 
 #O(n^2) - returning the boolean early doesn't do anything to the nested loop situation, which is where the bulk of the time complexity is derived 
 
-p bad_two_sum?([7, 6, 6, 7], 14)
+# p bad_two_sum?([7, 6, 6, 7], 14)
 
 
 def okay_two_sum?(arr, target)
 
   arr.sort
+  i = 0 
+  j = arr.length
+  until arr[i] + arr[j] == target || i == arr.length - 1
+    return true 
+  end
+
+  return false
+  
 
 end
+
+p okay_two_sum?([7, 6, 6, 7], 14)
