0062f64cd9cda0e2948f593d61e9cb61016544f9
Colin <colin.reitman87@gmail.com>
2019-03-11 15:56:03 -0400

    motivity

--- W10D1/motivity/config/application.rb
+++ W10D1/motivity/config/application.rb
@@ -6,11 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MusicApp
+module Motivity
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.action_controller.default_protect_from_forgery = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -25,4 +25,6 @@ module MusicApp
         :request_specs => false
     end
   end
+
+  
 end
